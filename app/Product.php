05218440f6e5544061715292f37c2d66a2ecaee9
Uchiman <utsman.khabillah@gmail.com>
2020-11-10 08:50:39 +0700

    first commit

---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function pesanan_details()
+    {
+        return $this->hasMany(PesananDetail::class, 'product_id', 'id');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('role_id')->default(1);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('alamat')->nullable();
+            $table->integer('no_hp')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
